e8e20d40f1e63bab1a05ecfc4a910f3229c52202
namancg <naman.c@perfios.com>
2021-10-07 20:25:55 +0530

    REFACTORED: regex pattern to make sure there are no alphabets in the start

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -5,3 +5,8 @@ let pinCodeRegEx = RegExp('[0-9]{6}')
 arguments.forEach(pinCode => {
 console.log(pinCode+"",pinCodeRegEx.test(pinCode))
  });
+
+pinCodeRegEx = RegExp('^[0-9]{6}$')
+arguments.forEach(pinCode => {
+console.log(pinCode+"",pinCodeRegEx.test(pinCode))
+ });
