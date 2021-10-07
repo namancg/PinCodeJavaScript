e1d3a3a173ec8b9cd2795cb4723e17de3d42b508
namancg <naman.c@perfios.com>
2021-10-07 21:03:55 +0530

    REFACTORED: regex pattern to allow space between the pin code

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -15,3 +15,8 @@ console.log(pinCode+"",pinCodeRegEx.test(pinCode))
 arguments.forEach(pinCode => {
 console.log(pinCode+"",pinCodeRegEx.test(pinCode))
  });
+
+pinCodeRegEx = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$')
+arguments.forEach(pinCode => {
+console.log(pinCode+"",pinCodeRegEx.test(pinCode))
+ });
