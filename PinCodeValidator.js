d099835ac66d58ce75ac7ec6ef04b18da2fef357
namancg <naman.c@perfios.com>
2021-10-07 20:22:35 +0530

    ADDED: code to check if pin code consists of 6 digits

---
+++ PinCodeValidator.js
@@ -0,0 +1,7 @@
+let arguments= process.argv;
+arguments.splice(0,2);
+
+let pinCodeRegEx = RegExp('[0-9]{6}')
+arguments.forEach(pinCode => {
+console.log(pinCode+"",pinCodeRegEx.test(pinCode))
+ });
